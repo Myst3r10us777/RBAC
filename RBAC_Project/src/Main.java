685416c6d2d4ed4666bbc6af442644ee1656d89c
My Name <I@example.com>
2026-02-21 17:46:57 +0700

    Add Permission PodPynkt(1.2)

--- RBAC_Project/src/Main.java
+++ RBAC_Project/src/Main.java
@@ -38,5 +38,7 @@ public class Main{
     }
     public static void main(String[] args){
         validTest();
+        Permission per = new Permission("Read", "users", "Читаем пользователя");
+        System.out.println(per.format());
     }
 }
